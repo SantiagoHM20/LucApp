179391bbcf9e48314178696eb9399a4766eee5e0
SantiagoHM20 <santiagohurtadom2024@gmail.com>
2025-08-11 23:26:05 -0500

    Limpieza de archivos no utilizados

--- js/modules/alertModule.js
+++
@@ -1,6 +0,0 @@
-﻿export function checkBudgetLimit(transactions, limit) {
-  const expenses = transactions
-    .filter(t => t.category === 'Gasto')
-    .reduce((sum, t) => sum + t.amount, 0);
-  return expenses >= limit;
-}
--- js/modules/filterModule.js
+++
@@ -1,6 +0,0 @@
-﻿export function applyFilters(transactions, { description, category }) {
-  return transactions.filter(t => {
-    return (!description || t.description.includes(description)) &&
-           (!category || t.category === category);
-  });
-}
